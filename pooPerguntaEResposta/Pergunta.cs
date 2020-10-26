1a352fdbcaeef31c3435367c409be397701a06cd
Daniel Victor <daniel.silva@apinteractive.com.br>
2020-10-26 14:09:58 -0300

    Propriedades simplificadas

---
+++ pooPerguntaEResposta/Pergunta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pooPerguntaEResposta
+{
+    public class Pergunta
+    {
+		public Pergunta()
+		{
+			this.Texto = "";
+			this.Resposta = "";
+		}
+		public Pergunta(string texto, string resposta)
+		{
+			this.Texto = texto;
+			this.Resposta = resposta;
+		}
+		public string Texto { get; set; }
+		public string Resposta { get; set; }
+
+	}
+}
